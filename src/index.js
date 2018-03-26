a91f0f1be5b3a72621c6fd26a2788b0264cf273d
Jo Meenen <jo@meenen.de>
2018-03-26 11:12:29 +0200

    Add babel-polyfill for async support

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import MainRouter from './MainRouter'
+require('babel-polyfill')
 
 // eslint-disable-next-line no-undef
 ReactDOM.render(<MainRouter />, document.getElementById('app-root'))
