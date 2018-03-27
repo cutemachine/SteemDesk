15c9cfe53ae45a7b965d8edadb1527d5bece4b47
Jo Meenen <jo@meenen.de>
2018-03-27 14:54:49 +0200

    Remove comments

--- src/state/steem/operations.js
+++ src/state/steem/operations.js
@@ -18,32 +18,11 @@ const usernameSubmitted = (name) => async (dispatch) => {
     if (!account) { throw new Error('Sorry, no account found. Minimum 3 chars, no uppercase.') }
     dispatch(usernameStatusChanged('VALID'))
     dispatch(reputationSet(formatReputation(account.reputation)))
-    console.log('Account', account)
-    // setAccount(account)
   } catch (error) {
     dispatch(usernameStatusChanged('INVALID'))
     dispatch(errorOccurred(error.message))
-    console.log(error.message)
-    // actions.error.set(error.message)
   }
 }
-
-// requestAccount: (name) => async (state, actions) => {
-//   try {
-//     const [account] = await steem.api.getAccountsAsync([name])
-//     if (!account) { throw new Error('Sorry, no account found. Minimum 3 chars, no uppercase.')}
-//     console.log('Account', account)
-//     actions.setAccount(account)
-//     actions.accountStatus('VALID')
-//     actions.requestDynamicGlobalProperties()
-//     actions.reputation(account.reputation)
-//     actions.requestFollowCount()
-//     actions.requestAccountHistory(name)
-//   } catch (error) {
-//     actions.accountStatus('INVALID')
-//     actions.error.set(error.message)
-//   }
-// },
 
 export default {
   usernameStatusChanged,
