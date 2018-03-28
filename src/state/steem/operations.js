833e9d99477ca241d528d5c1bc0b5e27c70ec59d
Jo Meenen <jo@meenen.de>
2018-03-28 17:37:30 +0200

    Make calls to Steem blockchain parallel

--- src/state/steem/operations.js
+++ src/state/steem/operations.js
@@ -17,39 +17,28 @@ const {
 // Thunks
 const usernameSubmitted = (name) => async (dispatch, getState) => {
   dispatch(usernameStatusChanged('VALIDATING'))
-  // account
   try {
-    const [account] = await steem.api.getAccountsAsync([name])
-    if (!account) { throw new Error('Sorry, no account found. Minimum 3 chars, no uppercase.') }
+    let [accounts, followCount, delegations, dynamicGlobalProperties] = await Promise.all([
+      steem.api.getAccountsAsync([name]),
+      steem.api.getFollowCountAsync(name),
+      steem.api.getVestingDelegationsAsync(name, -1, 100),
+      steem.api.getDynamicGlobalPropertiesAsync()
+    ])
+    if (!accounts[0]) { throw new Error('Sorry, no account found. Minimum 3 chars, no uppercase.') }
     dispatch(usernameStatusChanged('VALID'))
-    dispatch(reputationSet(formatReputation(account.reputation)))
+    dispatch(reputationSet(formatReputation(accounts[0].reputation)))
     dispatch(uiActions.addFlag(`Welcome ${name}`, 'SteemDesk loves you!'))
-  } catch (error) {
-    dispatch(usernameStatusChanged('INVALID'))
-    dispatch(errorOccurred(error.message))
-  }
-  // follow count
-  try {
-    const followCount = await steem.api.getFollowCountAsync(name)
+
     if (!followCount) { throw new Error('Sorry, could not get follow count for user.') }
     dispatch(followCountSet(followCount))
-  } catch (error) {
-    dispatch(errorOccurred(error.message))
-  }
-  // delegations
-  try {
-    const delegations = await steem.api.getVestingDelegationsAsync(name, -1, 100)
+
     if (!delegations) { throw new Error('Sorry, could not get delegations for user.') }
     dispatch(delegationsSet(delegations))
-  } catch (error) {
-    dispatch(errorOccurred(error.message))
-  }
-  // dynamic global propeties
-  try {
-    const dynamicGlobalProperties = await steem.api.getDynamicGlobalPropertiesAsync()
+
     if (!dynamicGlobalProperties) { throw new Error('Sorry, could not get dynamic global properties.') }
     dispatch(dynamicGlobalPropertiesSet(dynamicGlobalProperties))
   } catch (error) {
+    dispatch(usernameStatusChanged('INVALID'))
     dispatch(errorOccurred(error.message))
   }
 }
