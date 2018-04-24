6b0cdd0ba032f9fef7c13882a5234c539b4a7571
Jo Meenen <jo@meenen.de>
2018-04-24 08:44:02 +0200

    Improve layout of Dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import last from 'lodash.last'
 import Page, { Grid, GridColumn } from '@atlaskit/page'
+import { gridSize } from '@atlaskit/theme'
 import ErrorIcon from '@atlaskit/icon/glyph/error'
 import Banner from '@atlaskit/banner'
 import Spinner from '@atlaskit/spinner'
@@ -14,6 +14,13 @@ import Level from '../components/Level'
 import LevelItem from '../components/LevelItem'
 import Heading from '../components/Heading'
 import Title from '../components/Title'
+import styled from 'styled-components'
+
+const Padding = styled.div`
+  margin-top: ${gridSize() * 4}px;
+  margin-bottom: ${gridSize() * 4}px;
+  padding-bottom: ${gridSize() * 3}px;
+`
 
 class Dashboard extends Component {
   static propTypes = {
@@ -34,52 +41,62 @@ class Dashboard extends Component {
           <PageHeaderWithUserInput title='Dashboard' />
           <Grid layout='fluid'>
             <GridColumn medium={6}>
-              <Level>
-                <LevelItem>
-                  <Heading>Steem</Heading>
-                  { // Show spinner when price history is loading
-                    (this.props.priceHistoryStatus === 'LOADING')
-                      ? <Spinner size='small' />
-                      : <Title>{this.props.steemPrice}</Title>
-                  }
-                </LevelItem>
-              </Level>
+              <Padding>
+                <Level>
+                  <LevelItem>
+                    <Heading>Steem</Heading>
+                    { // Show spinner when price history is loading
+                      (this.props.priceHistoryStatus === 'LOADING')
+                        ? <Spinner size='small' />
+                        : <Title>{this.props.steemPrice}</Title>
+                    }
+                  </LevelItem>
+                </Level>
+              </Padding>
             </GridColumn>
             <GridColumn medium={6}>
-              <Level>
-                <LevelItem>
-                  <Heading>SBD</Heading>
-                  { // Show spinner when price history is loading
-                    (this.props.priceHistoryStatus === 'LOADING')
-                      ? <Spinner size='small' />
-                      : <Title>{this.props.sbdPrice}</Title>
-                  }
-                </LevelItem>
-              </Level>
+              <Padding>
+                <Level>
+                  <LevelItem>
+                    <Heading>SBD</Heading>
+                    { // Show spinner when price history is loading
+                      (this.props.priceHistoryStatus === 'LOADING')
+                        ? <Spinner size='small' />
+                        : <Title>{this.props.sbdPrice}</Title>
+                    }
+                  </LevelItem>
+                </Level>
+              </Padding>
             </GridColumn>
             <GridColumn medium={4}>
-              <Level>
-                <LevelItem>
-                  <Heading>Reputation</Heading>
-                  <Title>{this.props.reputation}</Title>
-                </LevelItem>
-              </Level>
+              <Padding>
+                <Level>
+                  <LevelItem>
+                    <Heading>Reputation</Heading>
+                    <Title>{this.props.reputation}</Title>
+                  </LevelItem>
+                </Level>
+              </Padding>
             </GridColumn>
             <GridColumn medium={4}>
-              <Level>
-                <LevelItem>
-                  <Heading>Followers</Heading>
-                  <Title>{this.props.followCount.follower_count}</Title>
-                </LevelItem>
-              </Level>
+              <Padding>
+                <Level>
+                  <LevelItem>
+                    <Heading>Followers</Heading>
+                    <Title>{this.props.followCount.follower_count}</Title>
+                  </LevelItem>
+                </Level>
+              </Padding>
             </GridColumn>
             <GridColumn medium={4}>
-              <Level>
-                <LevelItem>
-                  <Heading>Following</Heading>
-                  <Title>{this.props.followCount.following_count}</Title>
-                </LevelItem>
-              </Level>
+              <Padding>
+                <Level>
+                  <LevelItem>
+                    <Heading>Following</Heading>
+                    <Title>{this.props.followCount.following_count}</Title>
+                  </LevelItem>
+                </Level>
+              </Padding>
             </GridColumn>
           </Grid>
         </Page>
